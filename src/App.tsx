7815bc1a673a6465964cbe0c07185df6726050b4
Vitor S. <svavitor.dev@gmail.com>
2025-03-22 16:20:51 -0300

    adicionando visualizacao das cartas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState, useRef } from 'react'
+
+interface Card {
+  id: number;
+  front: string;
+  back: string;
+  reviews: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [isFrontVisible, setIsFrontVisible] = useState(true);
+  const [activeCard, setActiveCard] = useState<number>(0);
+
+  const toggleCard = () => {
+    setIsFrontVisible(!isFrontVisible);
+  };
+
+  const nextCard = () => {
+    if(activeCard+1 === cards.length) return;
+    setActiveCard(activeCard+1);
+  };
+
+  const prevCard = () => {
+    if(activeCard === 0) return
+    setActiveCard(activeCard-1);
+  };
+
+  useEffect(() => {
+
+    fetch("http://localhost:8080/api/v1/card")
+      .then((response) => {
+        if(!response.ok){
+          throw new Error("Falha ao pegar cartas");
+        }
+        return response.json();
+      })
+      .then((data) => {
+
+        setCards(data.map((card: any) => ( {
+          id: card.id,
+          front: card.front,
+          back: card.back,
+          reviews: card.reviews 
+        })));
+        setActiveCard(0)
+
+        setLoading(false);
+      })
+      .catch((err) =>{
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Cartas</h2>
+          <div key={cards[activeCard].id} onClick={toggleCard} style={{ cursor: "pointer"}}>
+            {isFrontVisible ? (
+              <div>{cards[activeCard].front}</div>
+            ) : (
+              <div>{cards[activeCard].back}</div>
+            )}
+          </div>
+          <button onClick={prevCard}>Anterior</button>
+          <button onClick={nextCard}>Próxima</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
